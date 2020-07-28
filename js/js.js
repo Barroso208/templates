14552c0ba27c53213089dc3072c48ae343cdde68
Barroso208 <Barroso208@users.noreply.github.com>
2020-07-29 00:28:50 +0200

    slider-añadido

--- js/js.js
+++ js/js.js
@@ -21,3 +21,68 @@ link1.onclick = function(){menu();}
 link2.onclick = function(){menu();}
 link3.onclick = function(){menu();}
 link4.onclick = function(){menu();}
+
+
+/* SLIDER */
+const botonAtras = document.getElementById("atras");
+let botonAdelante = document.getElementById("adelante");
+let imagen = document.getElementById("imagen");
+let level = 2;
+
+function sliderAdelante(){
+    console.log("vida");
+    switch (level) {
+        case 1:
+            imagen.setAttribute("src","https://cdn.pixabay.com/photo/2020/07/22/08/17/power-station-5428427__480.jpg");
+            level++;
+            break;
+
+        case 2:
+            imagen.setAttribute("src","https://cdn.pixabay.com/photo/2020/07/22/08/39/waterfall-5428468__480.jpg");
+            level++;
+            break;
+
+        case 3:
+            imagen.setAttribute("src","https://cdn.pixabay.com/photo/2016/10/18/21/28/seljalandsfoss-1751463__480.jpg");
+            level++;
+            break;
+            
+        case 4: 
+            imagen.setAttribute("src","https://cdn.pixabay.com/photo/2017/03/02/16/54/iceland-2111811__480.jpg")
+            level = 1;
+            break;
+    }
+    console.log(level)
+    return level;
+}
+
+botonAdelante.addEventListener('click',sliderAdelante);
+
+function sliderAtras(){
+    console.log("vida2");
+    switch (level) {
+        case 1:
+            imagen.setAttribute("src","https://cdn.pixabay.com/photo/2020/07/22/08/17/power-station-5428427__480.jpg");
+            level = 4;
+            break;
+
+        case 2:
+            imagen.setAttribute("src","https://cdn.pixabay.com/photo/2020/07/22/08/39/waterfall-5428468__480.jpg");
+            level--;
+            break;
+
+        case 3:
+            imagen.setAttribute("src","https://cdn.pixabay.com/photo/2016/10/18/21/28/seljalandsfoss-1751463__480.jpg");
+            level--;
+            break;
+            
+        case 4: 
+            imagen.setAttribute("src","https://cdn.pixabay.com/photo/2017/03/02/16/54/iceland-2111811__480.jpg")
+            level--;
+            break;
+    }
+    console.log(level+"2")
+    return level;
+}
+
+botonAtras.addEventListener('click',sliderAtras);
