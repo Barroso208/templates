967ad980a7e10183ff264202e5c293910be2b421
Barroso208 <Barroso208@users.noreply.github.com>
2020-07-29 23:12:05 +0200

    mejora del slider2

--- js/js.js
+++ js/js.js
@@ -29,70 +29,43 @@ let botonAdelante = document.getElementById("adelante");
 let imagen = document.getElementById("imagen");
 let level = 1;
 
-function sliderAdelante(){
-    console.log(level)
+
+/* Funciones para determinar que valor debe tener la variable level. */
+function ahead(){
     level++;
-    switch (level) {
-        case 1:
-            imagen.setAttribute("src","https://cdn.pixabay.com/photo/2020/07/22/08/17/power-station-5428427__480.jpg");
-            
-            break;
-
-        case 2:
-            imagen.setAttribute("src","https://cdn.pixabay.com/photo/2020/07/22/08/39/waterfall-5428468__480.jpg");
-            
-            break;
-
-        case 3:
-            imagen.setAttribute("src","https://cdn.pixabay.com/photo/2016/10/18/21/28/seljalandsfoss-1751463__480.jpg");
-
-            break;
-            
-        case 4: 
-            imagen.setAttribute("src","https://cdn.pixabay.com/photo/2017/03/02/16/54/iceland-2111811__480.jpg")
-            
-            break;
-        case 5:
-            level = 0;
-            sliderAdelante();
-            break;
+    if (level === 5){
+        level = 1;
     }
-    console.log(level)
-    return level;
+    changeImage();
 }
-
-botonAdelante.addEventListener('click',sliderAdelante);
-
-function sliderAtras(){
-    console.log(level)
+function behind(){
     level--;
+    if (level === 0){
+        level = 4;
+    }
+    changeImage();
+}
+/* Función para cargar la imagen correspondiente a cada número */
+function changeImage(){
     switch (level) {
-        case 0:
-            level = 5;
-            sliderAtras();
-            break;
         case 1:
             imagen.setAttribute("src","https://cdn.pixabay.com/photo/2020/07/22/08/17/power-station-5428427__480.jpg");
-            
             break;
 
         case 2:
-            imagen.setAttribute("src","https://cdn.pixabay.com/photo/2020/07/22/08/39/waterfall-5428468__480.jpg");
-            
+            imagen.setAttribute("src","https://cdn.pixabay.com/photo/2020/07/22/08/39/waterfall-5428468__480.jpg");         
             break;
 
         case 3:
             imagen.setAttribute("src","https://cdn.pixabay.com/photo/2016/10/18/21/28/seljalandsfoss-1751463__480.jpg");
-            
             break;
-            
+
         case 4: 
-            imagen.setAttribute("src","https://cdn.pixabay.com/photo/2017/03/02/16/54/iceland-2111811__480.jpg")
-            
+            imagen.setAttribute("src","https://cdn.pixabay.com/photo/2017/03/02/16/54/iceland-2111811__480.jpg");
             break;
     }
-    console.log(level)
-    return level;
 }
+/* Eventos para ejecutar las variables que determinan el valor de level y por tanto determinan que imagen se va a cargar */
+botonAdelante.addEventListener('click',ahead);
 
-botonAtras.addEventListener('click',sliderAtras);
+botonAtras.addEventListener('click',behind);
