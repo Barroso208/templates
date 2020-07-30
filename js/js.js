b2f32abc13bebf04fb6618c39f12ca15704a965e
Barroso208 <Barroso208@users.noreply.github.com>
2020-07-30 12:17:07 +0200

    mejorando el nav en mobil

--- js/js.js
+++ js/js.js
@@ -1,9 +1,5 @@
 let linksContainer = document.getElementById("links-container");
 let iconoMenu      = document.getElementById("menu");
-const link1          = document.getElementById("link1");
-const link2          = document.getElementById("link2");
-const link3          = document.getElementById("link3");
-const link4          = document.getElementById("link4");
 
 function menu(){
     if(window.screen.width<1090){
@@ -15,13 +11,9 @@ function menu(){
         }
     }
 }
-
 iconoMenu.onclick = function(){menu();}
-link1.onclick = function(){menu();}
-link2.onclick = function(){menu();}
-link3.onclick = function(){menu();}
-link4.onclick = function(){menu();}
-
+linksContainer.addEventListener('click',listClick);
+function listClick(e){menu();}
 
 /* SLIDER */
 const botonAtras = document.getElementById("atras");
